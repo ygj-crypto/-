0e8b19fcd0ba1e8c2636aa406780bd04b787cfd2
ygj-crypto <51847161+ygj-crypto@users.noreply.github.com>
2019-07-23 15:30:36 +0800

    Update HuSplit.h

--- HuSplit.h
+++ HuSplit.h
@@ -4,20 +4,6 @@
 #include "HuSplitTbl.h"
 #include "HuDefine.h"
 
-struct CardValue
-{
-	int32_t value;
-	int32_t num;
-	void clear(){
-		value = 0;
-		num = 0;
-	}
-};
-
-//typedef map<uint32_t,uint32_t> TblMap;
-
-typedef vector<CardValue> TblMap;
-
 namespace NHuSplit
 {
 
